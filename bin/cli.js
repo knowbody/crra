143b0f4e0999810a4c57c4cc2c958e5973ede17d
Mahen Seneviratne <mseneviratne@innovu.com>
2017-06-06 01:36:30 -0400

    make changes to cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,20 +1,15 @@
 #!/usr/bin/env node
 
-const program = require('commander')
-const spawn = require('child_process').spawn;
-const package = require('../package.json');
-const chalk = require('chalk');
+const program = require('commander');
 
 const build = require('./build');
+const pkg = require('../package.json');
 
 program
-.version(package.version)
-  .usage('<keywords>')
-  .option('-n, --new, [name]', 'Create the application')
-  .parse(process.argv);
+  .version(pkg.version);
 
-if (program.args.length === 1) {
-  spawn(build(program.args[0]), { shell: true, stdio: 'inherit' });
-} else if (program.args.length < 1) {
-  console.log(chalk.red('Please supply a name for your new Reason React app.'));
-}
+program
+  .command('new [name]', { isDefault: true })
+  .action(name => build(name));
+
+program.parse(process.argv);
