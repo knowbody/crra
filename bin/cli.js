b326c7a932102966b0e4494cf7ffd9c33219d374
Nader Dabit <dabit3@gmail.com>
2017-06-14 02:05:48 -0700

    new CLI with templates, upgrade to rr2
    * updated to take interop flag
    
    * upgrade to rr2 and add templates
    
    upgrade to rr2 and add templates

--- bin/cli.js
+++ bin/cli.js
@@ -3,13 +3,48 @@
 const program = require('commander');
 
 const build = require('./build');
+const help = require('./help');
 const pkg = require('../package.json');
+const chalk = require('chalk');
+
+let projectName;
 
 program
-  .version(pkg.version);
+  .version(pkg.version)
+  .arguments('<project-directory>')
+  // usage is used for the --help command
+  .usage(`${chalk.red('<project-directory>')} [options]`)
+  .option('-b, --basic', 'Create a basic template')
+  .option('-i, --interop', 'Create an interop template')
+  .action(name => {
+    projectName = name;
+  })
 
-program
-  .command('new [name]', { isDefault: true })
-  .action(name => build(name));
+program.on('--help', function() {
+  help();
+});
 
 program.parse(process.argv);
+
+if (program.basic) {
+  build(projectName);
+} else if (program.interop) {
+  build(projectName, 'interop')
+} else if (projectName) {
+  build(projectName);
+}
+
+if (typeof projectName === 'undefined') {
+  console.error('Please specify the project directory:');
+  console.log(
+    `  ${chalk.green(program.name())} ${chalk.red('<project-directory>')}`
+  );
+  console.log();
+  console.log('For example:');
+  console.log(`  ${chalk.green(program.name())} ${chalk.red('my-reason-app')}`);
+  console.log();
+  console.log(
+    `Run ${chalk.green(`${program.name()} --help`)} to see all options.`
+  );
+  process.exit(1);
+}
---
+++ templates/interop/README.md
@@ -0,0 +1,8 @@
+# Create Reason React App
+
+## Usage
+
+- **`yarn start`**: Start up the development environment
+- **`yarn build`**: Build for production
+- **`yarn clean`**: Clean the build folder
+- **`yarn test`**: Run Jest tests
