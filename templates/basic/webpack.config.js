f13217b9331fb6d4bebb1c06a9c3aedd2ffbc847
Theodore Zilist <tzilist@gmail.com>
2017-06-15 00:05:23 -0700

    [WIP] - Improve display errors  (#22)
    * reset master
    
    * merge
    
    * added loggin features for webpack and bsb
    
    * add compiling status to indicator
    
    * added cool top header
    
    * overlay now working for JS
    
    * remove old files
    
    * exit on webpack error
    
    * remove old files
    
    * decide between yarn and npm
    
    * small syntax changes

--- templates/basic/webpack.config.js
+++ templates/basic/webpack.config.js
@@ -3,14 +3,21 @@ const webpack = require('webpack');
 const WriteFilePlugin = require('write-file-webpack-plugin');
 
 module.exports = {
-  entry: './lib/js/src/index',
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './lib/js/src/index.js',
+  ],
   output: {
     filename: '[name].js',
     path: path.join(__dirname, './public'),
     publicPath: '/public'
   },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'public')
-  },
-  plugins: [new WriteFilePlugin()]
+  plugins: [
+    new WriteFilePlugin({
+      log: false,
+      test: /main\.js/,
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ]
 };
